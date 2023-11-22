4811114614b7aa7d88845ad23b1cedc6c9181aff
ldzanami <132527406+ldzanami@users.noreply.github.com>
2023-11-22 19:24:13 +0700

    Update BusClass.py

--- BusClass.py
+++ BusClass.py
@@ -21,10 +21,10 @@ class Bus():
                 while nexth not in ras_keys_int:
                     if nexth < max(ras_keys_int):
                         nexth += 1
-                    else: nexth = min(ras_keys_int)
+                    else: nexth = min(ras_keys_int); nextm = min(sorted(map(int, self.ras[str(nexth)])))
                 for i in self.ras[str(nexth)]:
                     i = int(i)
-                    if cur.hour == nexth and cur.minute <= i or nexth == min(self.ras) and nexth == max(self.ras):
+                    if cur.hour == nexth and cur.minute <= i:
                         nextm = i
                         break
                     elif cur.hour != nexth: nextm = i; break
